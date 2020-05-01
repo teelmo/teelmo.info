dc12f731cf24a28c8c53696c279f258932f67d5f
Teemo Tebest <teemo.tebest@gmail.com>
2020-05-01 13:36:01 +0200

    Updates

--- src/jsx/App.jsx
+++ src/jsx/App.jsx
@@ -32,7 +32,7 @@ class App extends Component {
         <h3><Typist cursor={{show: false,blink: true,element: '_',hideWhenDone: true, hideWhenDoneDelay: 0}}>Moro! Olen Teemo, ihminen.</Typist></h3>
         <div className={style.container}>
           <h2>Ura</h2>
-          <p>Olen koulutukseltani tietotekniikan diplomi-insinööri. Opiskelin Tampereella, pääaineenani oli Hypermedia ja valmistuin keväällä 2010. Vuodesta 2012 olen ollut Ylellä ja tällä hetkellä asun Genevessä ja työskentelen EBU:lla datajournalistina.</p>
+          <p>Olen koulutukseltani tietotekniikan diplomi-insinööri. Opiskelin Tampereella, pääaineenani oli Hypermedia ja valmistuin keväällä 2010. Vuodesta 2012 olen ollut Ylellä ja tällä hetkellä asun Genevessä ja työskentelen EBU:lla datajournalistina. Minut löytää myös <a href="https://fi.wikipedia.org/wiki/Teemo_Tebest">Wikipediasta</a>.</p>
           <data><label>TTY</label> <span>2009–2011</span></data>
           <data><label>Yle</label> <span>2012–2019</span></data>
           <data><label>EBU</label> <span>2019–</span></data>
@@ -54,12 +54,22 @@ class App extends Component {
           <data><label>10 km</label> <span>46min 52sek</span></data>
           <data><label>5 km</label> <span>21min 43sek</span></data>
           <data><label>Cooper</label> <span>3,02km</span></data>
-          <data><label>HCP</label> <span>17,2</span></data>
+          <data><label>HCP</label> <span>17,2 (NA)</span></data>
         </div>
         <div className={style.container}>
           <h2>Statster</h2>
-          <p>Vuonna 2007 perustin verkkopalvelun nimeltä Statster. Sen avulla pidän kirjaa kuunnelluista albumeistani, joita on kertynyt reilut 26&shy;000.</p>
+          <p>Vuonna 2007 perustin verkkopalvelun nimeltä Statster. Sen avulla pidän kirjaa kuunnelluista albumeistani, joita on kertynyt jo reilut 26&shy;000.</p>
           <data><label>Statster</label> <span><a href="https://statser.info/user/teelmo/">@teelmo</a></span></data>
+        </div>
+         <div className={style.container}>
+          <h2>Mediassa</h2>
+          <p>Tässä poimintoja kun minut on mainittu mediassa ja julkaisuistani. Olen kerännyt artikkeleita myös <a href="https://github.com/teelmo/newsstories">Githubiin</a>.</p>
+          <data><label>Apps4Finland</label> <span><a href="https://arcticstartup.com/apps4finland-winners-take-advantage-of-finlands-open-data/">Datavaalit</a></span></data>
+          <data><label>GEN</label> <span><a href="https://yle.fi/aihe/artikkeli/2016/04/18/yle-uutisten-joukkue-voittoisa-editors-labin-hackathon-tapahtumassa-helsingissa">Beef</a></span></data>
+          <data><label>NODA</label> <span><a href="https://yle.fi/aihe/artikkeli/2016/04/25/pohjoismainen-datajournalismipalkinto-ylen-kandideitti-vaalikoneelle">Kandideitti</a></span></data>
+          <data><label>Aamu-TV</label> <span><a href="https://areena.yle.fi/1-4071161">Vanhusvahti</a></span></data>
+          <data><label>Nordicom</label> <span><a href="https://www.nordicom.gu.se/sites/default/files/kapitel-pdf/tebest.pdf">Paperi</a></span></data>
+          <data><label>HRI</label> <span><a href="https://hri.fi/fi/ylen-datajutut-syntyvat-tekstieditorilla-taulukkolaskennalla-ja-pythonilla/">Haastattelu</a></span></data>
         </div>
         <div className={style.container}>
           <h2>Laitteisto</h2>
@@ -69,8 +79,9 @@ class App extends Component {
           <data><label>Kello</label> <span>Apple Watch 4</span></data>
           <data><label>Palvelin</label> <span>Ubuntu</span></data>
           <data><label>Konsoli</label> <span>Xbox 360</span></data>
-          <data><label>Kuulokkeet</label> <span>Beoplay H9i</span></data>
           <data><label>Kaiuttimet</label> <span>Dali ZENSOR 5.1</span></data>
+          <data><label>Vahvistin</label> <span>Marantz NR1403</span></data>
+          <data><label>Kuulokkeet</label> <span>Beoplay H9i</span></data>
           <data><label>Mikrofoni</label> <span>Røde NT</span></data>
           <data><label>Kamera</label> <span>Canon EOS 800D</span></data>
           <data><label>Lumilauta</label> <span>Burton Ripcord</span></data>
